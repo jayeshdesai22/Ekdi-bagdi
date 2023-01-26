f11c157ba38bd13107ceb9891ed876492a6dee1f
jdesa <jdesai087@gmail.com>
2023-01-26 13:45:40 +0530

    Adding sound and change background

---
+++ index.js
@@ -0,0 +1,100 @@
+var secretArr = []
+var started = false;
+
+$(document).keypress(function(){
+    if(!started){
+        var Jayesh =     prompt("JAYESH, Enter your secret number");
+var Ashish = prompt("ASHISH,Enter your secret number");
+var Dharmesh = prompt("DHARMESH, Enter your secret number");
+var Manish = prompt("MANISH, Enter your secret number");
+var Shubham = prompt("SHUBHAM, Enter your secret number");
+var Vimal = prompt("VIMAL, Enter your secret number");
+$("h1").text("એકડી બગડી");
+       
+        started=true;
+}  
+
+
+
+
+secretArr.push(Jayesh);
+secretArr.push(Ashish);
+secretArr.push(Dharmesh);
+secretArr.push(Manish);
+secretArr.push(Shubham);
+secretArr.push(Vimal);
+
+
+
+
+$(".btn1").click(function(){
+
+    var userChoosenBtn = $(this).attr("id");
+ 
+    checking(userChoosenBtn);
+  
+       
+       outClass(userChoosenBtn);
+});
+})
+       
+    
+    
+  /*  function checking(userChoosenBtn) {
+       if (secretArr.includes(userChoosenBtn)) {
+        $("h1").text(userChoosenBtn + " is Out from Game") 
+       }
+          
+            } */           
+
+function makeSound()
+{
+    $("div").addClass("danger");
+ setTimeout(function()  {
+ $("div").removeClass("danger")
+
+    }, 1500);
+    var outSound = new Audio("sound/out.wav")
+    outSound.play();
+}      
+function checking(userChoosenBtn) {
+    if (secretArr.includes(userChoosenBtn)) {
+        switch (userChoosenBtn) {
+                case secretArr[0]:
+                            $("h1").text("Jayesh " + userChoosenBtn + " is Out from Game") 
+                            
+                            makeSound();
+                            
+                            break;
+                case secretArr[1]:
+                                $("h1").text("Ashish " + userChoosenBtn + " is Out from Game") 
+                                makeSound();
+                                break;
+                case secretArr[2]:
+                            $("h1").text("Dharmesh " + userChoosenBtn + " is Out from Game") ;
+                            makeSound();
+                            break;
+                case secretArr[3]:
+                            $("h1").text("Manish " + userChoosenBtn + " is Out from Game") 
+                            makeSound();
+                            break;
+                case secretArr[4]:
+                            $("h1").text("Shubham " + userChoosenBtn + " is Out from Game") 
+                            makeSound();
+                            break;
+                case secretArr[5]:
+                            $("h1").text("Vimal " + userChoosenBtn + " is Out from Game") 
+                            makeSound();
+                            break;
+                        default:
+                            break;
+                    }
+                 
+                }
+                   
+                     }  
+    
+  
+function outClass(userChoosenBtn){
+    $("#"+userChoosenBtn).addClass("pressed");
+}
